4ad8de590748bab14bdccabe537c2abf5511abf8
SwathiM77 <47594298+SwathiM77@users.noreply.github.com>
2024-08-06 00:54:57 +0530

    Time: 23 ms (21.82%), Space: 28.2 MB (5.91%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<long long,int> map; 
+        for(int i=0;i<nums.size();i++){
+            map[nums[i]]++;
+        }
+        int maxi = -1, ans = 0;
+        for(auto i: map){
+            if( maxi < i.second){
+                ans = i.first;
+                maxi = i.second;
+            }
+        }
+        return ans;
+    }
+};
