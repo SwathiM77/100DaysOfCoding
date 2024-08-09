089d8d0b4d4df4ce70f34d4ac4a6afca100451fb
SwathiM77 <47594298+SwathiM77@users.noreply.github.com>
2024-08-10 00:07:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=2;i<n;i++){
             if(prime[i]){
                 count++;
-                for(int j=2*i;j<n;j+=i){
+                for(int j=i*i;j<n;j+=i){
                     prime[j] = false;
                 }
             }
