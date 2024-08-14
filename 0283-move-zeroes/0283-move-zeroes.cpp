fcdf4dad4845f776f5903e58c7161dad87103a91
SwathiM77 <47594298+SwathiM77@users.noreply.github.com>
2024-08-14 22:39:07 +0530

    Time: 15 ms (68.01%), Space: 21.8 MB (58.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
+    void printArray(vector<int> arr){
+        for(int i:arr){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
     void moveZeroes(vector<int>& nums) {
+        /*
         int zero = -1;
         for(int i=0;i<nums.size();i++){
             if(nums[i] == 0){
@@ -13,6 +20,20 @@ public:
             if(nums[nonZero] != 0){
                 swap(nums[zero++], nums[nonZero]);
             }
+            cout<<"nonZero = "<<nonZero<<" zero = "<<zero<<endl;
+            printArray(nums);
+        }
+        */
+        int zero = 0, nonZero = 0;
+        int n = nums.size();
+        while(zero < n && nonZero < n){
+            //cout<<"Before, nonZero = "<<nonZero<<" zero = "<<zero<<endl;
+            if(nums[nonZero] != 0){
+                swap(nums[nonZero], nums[zero++]);
+            }
+            nonZero++;
+            //cout<<"After, nonZero = "<<nonZero<<" zero = "<<zero<<endl;
+            //printArray(nums);
         }
     }
 };
