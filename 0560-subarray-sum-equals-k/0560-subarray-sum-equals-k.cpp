0ac80313cb5839a710fdca94552629b105ba4bf3
SwathiM77 <47594298+SwathiM77@users.noreply.github.com>
2024-08-23 02:17:09 +0530

    Time: 111 ms (31.16%), Space: 53.9 MB (7.15%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int cnt = 0, sum = 0;
-        map<int,int>presum;
+        map<int, int> presum;
         presum[0] = 1;
         for(int i=0;i<nums.size();i++){
-            sum += nums[i];
+            sum  += nums[i];
             int rem = sum - k;
             cnt += presum[rem];
             presum[sum]++;
