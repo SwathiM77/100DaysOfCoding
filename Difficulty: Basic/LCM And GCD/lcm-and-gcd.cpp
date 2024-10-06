155c85e73f79916e983719cf537028d362c14eff
SwathiM77 <47594298+SwathiM77@users.noreply.github.com>
2024-10-06 21:06:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
@@ -6,19 +6,21 @@ using namespace std;
 class Solution {
   public:
     vector<long long> lcmAndGcd(long long A , long long B) {
-        long long a = A, b = B, lcm, gcd;
-        while(A != 0 && B != 0){
-            if(A > B) A = A % B;
-            else B = B % A;
+        // code here
+        long long a = A, b = B, gcd, lcm = 0;
+        while(A > 0 && B > 0){
+            if(A > B){
+                A = A % B;
+            }
+            else{
+                B = B % A;
+            }
         }
         if(A == 0) gcd = B;
         else gcd = A;
-        
-        if(gcd)
-        lcm = (a * b)/gcd;
-        
+        if(gcd != 0)
+            lcm = (a * b)/gcd;
         return {lcm, gcd};
-        // code here
     }
 };
 
