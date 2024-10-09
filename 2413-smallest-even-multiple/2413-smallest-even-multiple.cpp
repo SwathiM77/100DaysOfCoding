a46ee2ca7e2aabcb617e8f0e131beec398e72285
SwathiM77 <47594298+SwathiM77@users.noreply.github.com>
2024-10-09 23:17:49 +0530

    Time: 3 ms (36.45%), Space: 7.3 MB (99.36%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        if(n & 1) return n*2;
-        return n;
+        int lcm, gcd;
+        int m = 2;
+        int a=n, b=m;
+        while(n>0 && m>0){
+            if(n>m) n = n%m;
+            else m = m%n;
+        }
+        if(n == 0) gcd = m;
+        else gcd = n;
+        lcm = (a*b)/gcd;
+        return lcm;
     }
 };
